d884dc96158fbe750e05919ecb897aa41fff8d36
gjudilla <gjudilla@gmail.com>
2024-04-05 22:48:00 -0700

    reinstall packages

---
+++ index.js
@@ -0,0 +1,16 @@
+// importing modules and packages
+const inquirer = require ("inquirer");
+const fs = require ("fs");
+
+// TO DO: import shapes
+
+// TO DO: Write Questions for user to answer
+
+
+fs.writeFile('logo.svg', logo.renderSvg(), function (err) {
+    if (err) throw err;
+    console.log('logo.svg generated!');
+  });
+} );
+
+
