f3064a3cae7f4861f8ac48e27f8bcb986d63115c
gjudilla <gjudilla@gmail.com>
2024-04-10 22:27:19 -0700

    added to index.js

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
-// importing modules and packages
-const inquirer = require ("inquirer");
-const fs = require ("fs");
+// importing packages
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TO DO: import shapes
-const { Circle, Triangle, Square } = require('./lib/shapes'); 
+// importing shapes
+const { Circle, Triangle, Square } = require('./lib/shapes.js');
 
-// TO DO: Write Questions for user to answer
+// questions for user
 
-
-fs.writeFile('logo.svg', logo.renderSvg(), function (err) {
-    if (err) throw err;
-    console.log('logo.svg generated!');
+inquirer
+  .prompt([
+    {
+      type: 'text',
+      name: 'logoText',
+      message: 'Type in 3 letters to use for your logo.'
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'What color would you like your text to be? Enter color keyword or hexadecimal number.'
+    },
+    {
+      type: 'list',
+      name: 'logoShape',
+      message: 'What shape would you like to use for your logo?',
+      choices: ['Circle', 'Triangle', 'Square']
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'What color would you like your shape to be? Enter color keyword or hexadecimal number.'
+    },
+  ])
+  .then((answers) => {
+    const { logoShape } = answers;
+    let logo;
+    switch (logoShape) {
+      case 'Circle':
+        logo = new Circle(answers.logoText, answers.textColor, answers.shapeColor);
+        break;
+      case 'Triangle':
+        logo = new Triangle(answers.logoText, answers.textColor, answers.shapeColor);
+        break;
+      case 'Square':
+        logo = new Square(answers.logoText, answers.textColor, answers.shapeColor);
+        break;
+    }
+    fs.writeFile('./example/logo.svg', logo.renderSvg(), function (err) {
+      if (err) throw err;
+      console.log('logo.svg generated!');
+    });
   });
-} );
+
 
 
